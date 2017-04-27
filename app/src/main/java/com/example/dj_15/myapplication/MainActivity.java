145520ed1d64089d66d40e885af6df1fde0a0fb1
Syreith92 <carlottaforzagenoa@hotmail.it>
2017-04-27 09:36:33 +0200

    qweiuy

--- app/src/main/java/com/example/dj_15/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dj_15/myapplication/MainActivity.java
@@ -1,42 +1,16 @@
 package com.example.dj_15.myapplication;
 
-import android.content.DialogInterface;
-import android.content.res.Configuration;
-import android.os.PersistableBundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity{
-
-    private String lastFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState != null) {
-            LoginFragment login = (LoginFragment) getFragmentManager().findFragmentById(R.id.login_fragment);
-
-            if(login != null){
-                android.app.FragmentTransaction trans = getFragmentManager().beginTransaction();
-                trans.add(R.id.frag_container, login).commit();
-            }else{
-                RegisterFragment reg = (RegisterFragment) getFragmentManager().findFragmentById(R.id.register_fragment);
-                if(reg != null){
-                    android.app.FragmentTransaction trans = getFragmentManager().beginTransaction();
-                    trans.add(R.id.frag_container, reg).commit();
-                }
-            }
-        }else{
+        if(savedInstanceState==null){
             RegisterFragment register = (RegisterFragment) getFragmentManager().findFragmentById(R.id.frag_container);
 
     @Override
