979723f4fc9c7291107db317e989f4248927d9ba
Syreith92 <carlottaforzagenoa@hotmail.it>
2017-04-27 11:05:50 +0200

    Nuova activity!!! YAY!!!

---
+++ app/src/main/java/com/example/dj_15/myapplication/LibraryActivity.java
@@ -0,0 +1,33 @@
+package com.example.dj_15.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Carlotta on 27/04/2017.
+ */
+
+public class LibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
