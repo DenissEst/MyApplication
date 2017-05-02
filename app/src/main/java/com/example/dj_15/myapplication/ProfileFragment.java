8d0cb8b6e5a299a9c169ba3484843185f9a64d7f
oneup94 <monica.sechi1994@gmail.com>
2017-05-02 13:07:58 +0200

    ExpandableView con stringhe finito + layout weight in ordine.

--- app/src/main/java/com/example/dj_15/myapplication/ProfileFragment.java
+++ app/src/main/java/com/example/dj_15/myapplication/ProfileFragment.java
@@ -5,7 +5,14 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Monica on 28/04/2017.
@@ -15,6 +22,10 @@ public class ProfileFragment extends Fragment {
 
     TextView username;
     TextView name;
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListDetail;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,28 @@ public class ProfileFragment extends Fragment {
         username.setText(getActivity().getIntent().getExtras().getString("myUsername"));
 
         name = (TextView) view.findViewById(R.id.nameprofile);
+
+        expandableListView = (ExpandableListView) view.findViewById(R.id.exlistviewProfile);
+        expandableListDetail = ExpandableListProfile.getData();
+        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+        expandableListAdapter = new CustomExpandableListProfile(this.getActivity(), expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener(){
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getActivity().getApplicationContext(), expandableListTitle.get(groupPosition)
+                        + "List Expanded.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener(){
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                Toast.makeText(getActivity().getApplicationContext(), expandableListTitle.get(groupPosition) + "->" + expandableListDetail.get(expandableListTitle.get(groupPosition)).get(childPosition), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
 
         return view;
     }
